34ac30cd07eeac94406095a0489c78c73697194f
Turovae <ekaterina.turova@mail.ru>
2023-01-19 01:57:15 +0700

    change tests

--- src/app/modules/__tests__/GameSavingLoader1.js
+++ src/app/modules/__tests__/GameSavingLoader1.js
@@ -1,11 +1,8 @@
 import GameSavingLoader from '../GameSavingLoader';
-import read from '../reader';
 
 jest.mock('../reader');
 
 test('test fail read', async () => {
-  jest.doMock('../reader');
-  read.mockReturnValueOnce();
   try {
     await GameSavingLoader.load();
   } catch (e) {
