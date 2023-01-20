342c80b5e226192ab239008c45b0fc7bcbd9f03e
Turovae <ekaterina.turova@mail.ru>
2023-01-21 01:02:05 +0700

    delete if...else from GameSavingLoader

--- src/app/modules/GameSavingLoader.js
+++ src/app/modules/GameSavingLoader.js
@@ -5,9 +5,6 @@ import read from './reader';
 export default class GameSavingLoader {
   static async load() {
     const data = await read();
-    if (!data) {
-      throw new Error('Data not readed!');
-    }
     const jsonString = await json(data);
     return new GameSaving(jsonString);
   }
--- src/app/modules/__tests__/GameSavingLoader1.js
+++ src/app/modules/__tests__/GameSavingLoader1.js
@@ -1,11 +1,20 @@
 import GameSavingLoader from '../GameSavingLoader';
+import read from '../reader';
 
 jest.mock('../reader');
 
 test('test fail read', async () => {
+  read.mockRejectedValue(new Error('Data not readed!'));
+  // Вариант try ... catch работает.
   try {
     await GameSavingLoader.load();
   } catch (e) {
     expect(e).toEqual(new Error('Data not readed!'));
   }
+  // Вариант toThrow так и не получился
+  // В логах теста видно, что пробрасывается заданная ошибка,
+  // но почему-то не перебрасывается в метод toThrow
+  // expect(async () => {
+  //   await GameSavingLoader.load();
+  // }).toThrow(new Error('Data not readed!'));
 });
