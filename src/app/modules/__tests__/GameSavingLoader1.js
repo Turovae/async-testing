ce6a563ed733a3aaac6ebe35682e12b869550173
Turovae <ekaterina.turova@mail.ru>
2023-01-21 23:08:25 +0700

    add mocking tests

--- src/app/modules/__tests__/GameSavingLoader1.js
+++ src/app/modules/__tests__/GameSavingLoader1.js
@@ -3,6 +3,10 @@ import read from '../reader';
 
 jest.mock('../reader');
 
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
 test('test fail read', async () => {
   read.mockRejectedValue(new Error('Data not readed!'));
   // Вариант try ... catch работает.
@@ -18,3 +22,62 @@ test('test fail read', async () => {
   //   await GameSavingLoader.load();
   // }).toThrow(new Error('Data not readed!'));
 });
+
+test('test fail read with toThrow', async () => {
+  read.mockRejectedValue(new Error('Data not readed!'));
+  expect(async () => {
+    await GameSavingLoader.load();
+  }).rejects.toThrow(new Error('Data not readed!'));
+});
+
+test('test mock resolved with mock', async () => {
+  read.mockImplementation(() => new Promise((resolve) => {
+    setTimeout(() => {
+      const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+      return ((input) => {
+        const buffer = new ArrayBuffer(input.length * 2);
+        const bufferView = new Uint16Array(buffer);
+        for (let i = 0; i < input.length; i += 1) {
+          bufferView[i] = input.charCodeAt(i);
+        }
+        resolve(buffer);
+      })(data);
+    }, 1000);
+  }));
+  const gameSaving = await GameSavingLoader.load();
+  expect(gameSaving).toEqual({
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1,
+      name: 'Hitman',
+      level: 10,
+      points: 2000,
+    },
+  });
+});
+
+// Все равно не работает сброс моков
+// ошибка [SyntaxError: Unexpected end of JSON input]
+// ни этот сброс не работает
+// read.mockReset();
+
+// test('GameSavingLoader test', async () => {
+// ни этот...
+//   read.mockReset();
+//   try {
+//     const gameSaving = await GameSavingLoader.load();
+//     expect(gameSaving).toEqual({
+//       id: 9,
+//       created: 1546300800,
+//       userInfo: {
+//         id: 1,
+//         name: 'Hitman',
+//         level: 10,
+//         points: 2000,
+//       },
+//     });
+//   } catch (e) {
+//     expect(e).toEqual(new Error('Data not readed!'));
+//   }
+// });
